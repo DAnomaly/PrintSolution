0f99693ac4b0978c10b8200c7bdac6524876f7ba
DAnomaly <parksh1219@naver.com>
2023-04-27 13:13:13 +0900

    SAMPLE (미완성)

---
+++ PCLProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCLProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Spire.Pdf.PdfDocument doc = new Spire.Pdf.PdfDocument())
+            {
+                doc.LoadFromFile(args[0]);
+                doc.SaveToFile("test.pcl", Spire.Pdf.FileFormat.PCL);
+            }
+
+        }
+    }
+}
