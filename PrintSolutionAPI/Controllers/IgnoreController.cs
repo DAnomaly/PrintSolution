ce9c9e4faa59b718f01fac4314f2962c79e5d863
DAnomaly <parksh1219@naver.com>
2023-05-11 16:50:07 +0900

    Add editing ignore.inf

--- PrintSolutionAPI/Controllers/IgnoreController.cs
+++ PrintSolutionAPI/Controllers/IgnoreController.cs
@@ -41,5 +41,68 @@ namespace PrintSolutionAPI.Controllers
 
             return result;
         }
+
+        /// <summary>
+        /// 무시 프린터 추가
+        /// </summary>
+        /// <param name="name">새로운 무시프린터명</param>
+        /// <returns>성공여부</returns>
+        [HttpGet]
+        [Route("Add")]
+        public IEnumerable<bool> Add(string name)
+        {
+            string content = System.IO.File.ReadAllText("ignore.inf");
+            content += "\r\n" + name;
+            System.IO.File.WriteAllText("ignore.inf", content);
+
+            yield return true;
+        }
+
+        /// <summary>
+        /// 무시 프린터 제거
+        /// </summary>
+        /// <param name="name">삭제할 무시프린터명</param>
+        /// <returns>성공여부</returns>
+        [HttpGet]
+        [Route("Remove")]
+        public IEnumerable<bool> Remove(string name)
+        {
+            string content = System.IO.File.ReadAllText("ignore.inf");
+            string[] lines = content.Split('\n');
+            List<string> lineList = lines.ToList();
+            bool delete = false;
+            for (int i = 0; i < lineList.Count; i++)
+            {
+                string printerName = lines[i];
+                if (printerName.Contains('\r'))
+                {
+                    printerName = printerName.Replace("\r", string.Empty);
+                }
+
+                if (printerName == name)
+                {
+                    delete = true;
+                    lineList.RemoveAt(i);
+                    break;
+                }
+            }
+
+            if (delete)
+            {
+                content = string.Empty;
+                for (int i = 0; i < lineList.Count; i++)
+                {
+                    if (content != string.Empty)
+                        content += "\r\n";
+                    content += lineList[i];
+                }
+
+                System.IO.File.WriteAllText("ignore.inf", content);
+
+                yield return true;
+            }
+
+            yield return false;
+        }
     }
 }
